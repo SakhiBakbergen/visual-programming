4b50f54c004c0c57bc31da1bb3bcf9f57a6006d1
MisterBakbergen <sakibakbergen@gmail.com>
2026-09-08 21:02:58 +0500

    Add files via upload

---
+++ Lab1_Task1/src/main/java/kz/atu/lab/HelloController.java
@@ -0,0 +1,13 @@
+package kz.atu.lab;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label lblName;
+    @FXML
+    private Label lblGroup;
+    @FXML
+    private Label lblSpec;
+}
